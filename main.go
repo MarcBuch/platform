dc8b84f0575fc5d0b74b92e3763432aa265a18b3
Marc Buchardt <marc.buchardt@sartorius.com>
2023-12-19 12:40:16 +0100

    feat: further build out CUE parsing and add a REST server

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"net/http"
 
 	"cuelang.org/go/cue"
 	"cuelang.org/go/cue/cuecontext"
@@ -9,17 +11,107 @@ import (
 )
 
 func main() {
+	http.HandleFunc("/", clusterHandler)
+	http.ListenAndServe(":8080", nil)
+}
+
+type ClusterConfig struct {
+	Name               string
+	Kubernetes_version string
+	Owner              string
+	Default_pool       PoolConfig
+	Network            NetworkConfig
+	Features           FeatureConfig
+}
+
+type PoolConfig struct {
+	Min_count int
+	Max_count int
+	Sku       string
+}
+
+type NetworkConfig struct {
+	Name string
+	Cidr string
+}
+
+type FeatureConfig struct {
+	External_secrets bool
+	Grafana          bool
+	Snyk             bool
+	Argo             bool
+	Flux             bool
+}
+
+type CueReader struct {
+	values cue.Value
+}
+
+// ReadFile reads a CUE file and returns a cue.Value
+func (reader CueReader) ReadFile(filepath string) (cue.Value, error) {
 	ctx := cuecontext.New()
-	f, err := parser.ParseFile("test.cue", nil)
+
+	f, err := parser.ParseFile(filepath, nil)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println(err) // TODO: Implement logging
+		return cue.Value{}, err
 	}
 
-	value := ctx.BuildFile(f)
-	fmt.Println(value.LookupPath(cue.ParsePath("name")))
-	// ctx := cuecontext.New()
+	values := ctx.BuildFile(f)
+	err = values.Validate()
+	if err != nil {
+		fmt.Println(err) // TODO: Implement logging
+		return cue.Value{}, err
+	}
+	return values, nil
+}
 
-	// v := ctx.CompileString(config)
-	// msg := v.LookupPath(cue.ParsePath("msg"))
-	// fmt.Println(msg)
+// GetValue looksup a path and returns a specific value
+func (reader CueReader) GetValue(path string) cue.Value {
+	return reader.values.LookupPath(cue.ParsePath(path))
+}
+
+func mapConfig(reader CueReader) ClusterConfig {
+	var clusterConfig ClusterConfig
+
+	clusterConfig.Name, _ = reader.GetValue("cluster.name").String()
+	clusterConfig.Kubernetes_version, _ = reader.GetValue("cluster.kubernetes_version").String()
+	clusterConfig.Owner, _ = reader.GetValue("cluster.owner").String()
+	minCount, _ := reader.GetValue("cluster.default_pool.min_count").Int64()
+	clusterConfig.Default_pool.Min_count = int(minCount)
+	maxCount, _ := reader.GetValue("cluster.default_pool.max_count").Int64()
+	clusterConfig.Default_pool.Max_count = int(maxCount)
+	clusterConfig.Default_pool.Sku, _ = reader.GetValue("cluster.default_pool.sku").String()
+	clusterConfig.Network.Name, _ = reader.GetValue("network.name").String()
+	clusterConfig.Network.Cidr, _ = reader.GetValue("network.cidr").String()
+	clusterConfig.Features.External_secrets, _ = reader.GetValue("features.external_secrets").Bool()
+	clusterConfig.Features.Grafana, _ = reader.GetValue("features.grafana").Bool()
+	clusterConfig.Features.Snyk, _ = reader.GetValue("features.snyk").Bool()
+	clusterConfig.Features.Argo, _ = reader.GetValue("features.argo").Bool()
+	clusterConfig.Features.Flux, _ = reader.GetValue("features.flux").Bool()
+
+	return clusterConfig
+}
+
+func clusterHandler(w http.ResponseWriter, r *http.Request) {
+	reader := CueReader{}
+	values, err := reader.ReadFile("test.cue")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	reader.values = values
+	clusterConfig := mapConfig(reader)
+	fmt.Println(clusterConfig)
+
+	jsonData, err := json.Marshal(clusterConfig)
+	if err != nil {
+		fmt.Println(err) // TODO: Implement logging
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	fmt.Printf("%s\n", jsonData)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(jsonData)
 }
--- test.cue
+++ test.cue
@@ -1,3 +1,24 @@
-name: "Marc's Test Subscription"
-connected: true
-online: false
+cluster: {
+	name:               "aks-dev-marc-test-123"
+	kubernetes_version: "1.27.3"
+	owner: "MarcBuch"
+
+	default_pool: {
+		min_count: 2
+		max_count: 6
+		sku:       "Standard_D2_v2"
+	}
+}
+
+network: {
+    name: "vnet-dev-marc-test-123"
+	cidr: "10.0.0.0/16"
+}
+
+features: {
+	external_secrets: true
+	grafana:          true
+	snyk:             true
+	argo:             false
+	flux:             false
+}
